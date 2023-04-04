1fe8dbe19aa482253b94821a513763ae6a042ad0
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-03 20:34:57 -0400

    nav working

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,9 +5,10 @@ import { HashLink } from 'react-router-hash-link';
 export default function NavBar(props) {
 
     const isLogin = props.isLogin
-    
-    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-    
+
+    const toggleButton = document.getElementsByClassName('toggle-button')[0]
+
+
     useEffect(() => {
         var navItems = document.getElementsByClassName("navItems");
         for (var i = 0; i < navItems.length; i++) {
@@ -17,13 +18,14 @@ export default function NavBar(props) {
                 this.className += " active";
             });
         }
-    }, [navbarLinks])
+    })
 
     const toggleNav = () => {
-        navbarLinks.classList.toggle('column')
-        console.log(window.innerWidth)
+        if (window.innerWidth < 735) {
+            const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+            navbarLinks.classList.toggle('column')
+        }
     }
-
 
     return (
         <>
@@ -35,15 +37,15 @@ export default function NavBar(props) {
                 </a>
                 <div class="navbar-links">
                     {/* <ul className="navBox"> */}
-                        <Link to="/" class="navItems active" onClick={() => toggleNav()}> Home </Link>
-                        <Link to="/menu" class="navItems" onClick={() => toggleNav()}> Menu </Link>
-                        <Link to="/order" class="navItems" onClick={() => toggleNav()}> Order </Link>
-                        <HashLink to="/#contact" class="navItems" smooth onClick={() => toggleNav()}> Contact Us </HashLink>
-                        {
-                            !isLogin ? <Link to="/account" class="navItems" onClick={() => toggleNav()}> Account </Link> :
-                                <Link to="/Profile" class="navItems" onClick={() => toggleNav()}> Profile </Link>
-                        }
-                        <Link to="/cart" class="navItems right" onClick={() => toggleNav()}> My Cart</Link>
+                    <Link to="/" class="navItems active" onClick={() => toggleNav()}> Home </Link>
+                    <Link to="/menu" class="navItems" onClick={() => toggleNav()}> Menu </Link>
+                    <Link to="/order" class="navItems" onClick={() => toggleNav()}> Order </Link>
+                    <HashLink to="/#contact" class="navItems" smooth onClick={() => toggleNav()}> Contact Us </HashLink>
+                    {
+                        !isLogin ? <Link to="/account" class="navItems" onClick={() => toggleNav()}> Account </Link> :
+                            <Link to="/Profile" class="navItems" onClick={() => toggleNav()}> Profile </Link>
+                    }
+                    <Link to="/cart" class="navItems right" onClick={() => toggleNav()}> My Cart</Link>
                     {/* </ul> */}
                 </div>
             </nav>
